320d808f2cd5367e5f6aa14109b157712d8810d9
lalitx17 <lalityadav.x17@gmail.com>
2023-11-14 15:28:58 -0600

    added HomeScreen and designed navbar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,31 @@
-import { type Config } from "tailwindcss";
-import { fontFamily } from "tailwindcss/defaultTheme";
+/** @type {import('tailwindcss').Config} */
 
-export default {
-  content: ["./src/**/*.tsx"],
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens:{
+      sm: '480px',
+      md: '768px', 
+      lg: '976px', 
+      xl: '1440px'
+    },
     extend: {
-      fontFamily: {
-        sans: ["var(--font-sans)", ...fontFamily.sans],
+      colors: {
+        purpleG: "rgba(121,105,192,1)",
+        blueG: "rgba(96,163,230,1)",
+        complementary: "#FFE9B1",
+        minWhite: "#FCFDF2",
+        secondaryBackground: "#262d36"
       },
+      fontFamily: {
+        primary: ["'Lato'", '"sans-serif"'],
+        complementry: ["'Montserrat'", 'sans-serif'],
+        white: ["'Playfair Display'", 'serif'],
+        secondary: ["'Poppins'", "sans-serif"],
+        inter: ["'Inter'", "sans-serif"]
+      }
     },
   },
-  plugins: [],
-} satisfies Config;
+  plugins: [require(`@tailwindcss/typography`)],
+};
+
