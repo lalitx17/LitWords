34d5a490e2fa05ce2239aa838c889409ecc2dad9
lalitx17 <lalityadav.x17@gmail.com>
2023-11-27 21:29:25 -0600

    added BlogCard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,12 +6,24 @@ import Image from "next/image";
 
 import logo from "../../public/image/litwords4.png";
 import quirkyLalit from "../../public/image/QuirkyLality2.jpg";
+import solitude from "../../public/image/solitude.jpg";
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>LitWords</title>
+        <style>
+          {`
+            .bg1{
+              background-image:url(${solitude.src});
+              background-repeat: no-repeat;
+              background-position: center;
+              background-size: cover;
+              height: 100vh;
+          }
+            `}
+        </style>
       </Head>
 
       <main>
@@ -21,6 +33,9 @@ export default function Home() {
           </div>
           <Navbar />
           <HomeScreen />
+          <div className="py-10">
+              <Blogcard />
+          </div>
         </div>
       </main>
     </>
@@ -101,10 +116,58 @@ function HomeScreen() {
       </div>
       <button
         type="button"
-        className="mb-2 me-2 mt-5 rounded-full border border-gray-200 bg-white px-10 py-2.5 text-md font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700"
+        className="text-md mb-2 me-2 mt-5 rounded-full border border-gray-200 bg-white px-10 py-2.5 font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700"
       >
         Alternative
       </button>
+    </div>
+  );
+}
+
+function Blogcard() {
+  return (
+    <div className="max-w-sm rounded-lg border border-gray-200 bg-white shadow dark:border-gray-700 dark:bg-gray-800 mx-4">
+      <a href="#">
+        <Image
+          src={solitude.src}
+          width={600}
+          height={400}
+          alt="solitude"
+          className="rounded-t-lg p-4" 
+        />
+      </a>
+      <div className="p-5">
+        <a href="#">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Noteworthy technology acquisitions 2021
+          </h5>
+        </a>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          Here are the biggest enterprise technology acquisitions of 2021 so
+          far, in reverse chronological order.
+        </p>
+        <a
+          href="#"
+          className="inline-flex items-center rounded-lg bg-blue-700 px-3 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Read more
+          <svg
+            className="ms-2 h-3.5 w-3.5 rtl:rotate-180"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 10"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M1 5h12m0 0L9 1m4 4L9 9"
+            />
+          </svg>
+        </a>
+      </div>
     </div>
   );
 }
