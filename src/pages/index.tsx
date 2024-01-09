13b96ec9c54cbb2b1275a977120600b3adccf781
lalitx17 <lalityadav.x17@gmail.com>
2024-01-08 22:51:01 -0600

    mvp for frontend

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -174,7 +174,7 @@ function Banner() {
         <div className="absolute inset-[60px] backdrop-blur-sm"></div>
 
         <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform text-center text-white">
-          <p className="mt-2 font-primary text-lg">“To go wrong in one's own way is better than to go right in someone else's.”</p>
+          <p className="mt-2 font-primary text-lg">“To go wrong in one&#39;s own way is better than to go right in someone else&#39;s.”</p>
         </div>
       </div>
     </div>
