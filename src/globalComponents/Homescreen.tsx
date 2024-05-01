a0935709f55481324d0b3f3d713aab6952d67a0f
lalitx17 <lalityadav.x17@gmail.com>
2024-04-30 23:25:44 -0500

    new schema added

--- src/globalComponents/Homescreen.tsx
+++ src/globalComponents/Homescreen.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 
 import quirkyLalit from "../../public/image/QuirkyLality2.jpg";
+import lalitx from "../../public/image/lalitx.jpg";
 
 function HomeScreen() {
     return (
