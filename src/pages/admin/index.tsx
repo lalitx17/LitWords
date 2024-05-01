39c5f0dccae13f44b20754b3b3276d64003604e4
lalitx17 <lalityadav.x17@gmail.com>
2024-05-01 17:46:03 -0500

    login and signup features added

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,7 +1,8 @@
 // src/components/SignupForm.tsx
-import React, { useState } from 'react';
-import { useRouter } from 'next/router';
-import cookie from 'js-cookie';
+import React, { useState } from "react";
+import { useRouter } from "next/router";
+import cookie from "js-cookie";
+import { api } from "~/utils/api";
 
 interface SignupFormData {
   username: string;
@@ -9,62 +10,161 @@ interface SignupFormData {
 }
 
 const SignupForm: React.FC = () => {
-const [formData, setFormData] = useState<SignupFormData>({
-    username: '',
-    password: '',
-});
+  const router = useRouter();
 
-const router = useRouter();
+  const [signupFormData, setSignupFormData] = useState<SignupFormData>({
+    username: "",
+    password: "",
+  });
 
-const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const signupHandle = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
-    setFormData((prevData) => ({
-        ...prevData,
-        [name]: value,
+    setSignupFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
     }));
-};
+  };
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const signUpMutaion = api.auth.signupMutation.useMutation({
+    onSuccess: async () => {
+      await router.push("/admin/editor");
+    },
+  });
+
+
+  const onSignupSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (formData.username == "lalitx17" && formData.password == "1234") {
-      cookie.set("lit", "xyxyxy" , { expires: 30 });
-      router.push("/admin/editor").catch((err) => console.error(err));
-    } else {
-        formData.username == "" || formData.password == "" ? alert("Please fill all the fields") : alert("Invalid Credentials");
+    if (!signUpMutaion.isLoading) {
+      if (signupFormData.username && signupFormData.password) {
+        signUpMutaion.mutate(signupFormData);
+      }
+    }
+  };
+
+
+  const [loginFormData, setloginFormData] = useState<SignupFormData>({
+    username: "",
+    password: "",
+  });
+
+  const loginHandle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setloginFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const loginMutation = api.auth.loginMutation.useMutation({
+    onSuccess: async (data) => {
+      console.log(data);
+      cookie.set("litwordRemembers", data.token, { expires: 1});
+      await router.push("/admin/editor");
+    },
+  });
+
+  const loginHandleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!loginMutation.isLoading) {
+      if (loginFormData.username && loginFormData.password) {
+        loginMutation.mutate(loginFormData);
+      }
     }
   };
 
   return (
-    <div className="flex justify-center items-center h-screen ">
-      <form onSubmit={handleSubmit} className='border-2 border-black w-1/5 p-4'>
-        <div className="mb-4">
-          <label htmlFor="username" className="block mb-2 text-sm font-semibold text-gray-600">Username</label>
-          <input
-            type="text"
-            id="username"
-            name="username"
-            value={formData.username}
-            onChange={handleChange}
-            className="w-full px-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:border-blue-500"
-          />
-        </div>
-        <div className="mb-6">
-          <label htmlFor="password" className="block mb-2 text-sm font-semibold text-gray-600">Password</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            className="w-full px-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:border-blue-500"
-          />
-        </div>
-        <button type="submit" className="w-full py-2 px-4 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600">
-          Sign Up
-        </button>
-      </form>
+    <div className="flex h-screen flex-row items-center justify-evenly">
+      <div className="w-1/5">
+        <form
+          onSubmit={onSignupSubmit}
+          className="w-full border-2 border-black p-4"
+        >
+          <div className="mb-4">
+            <label
+              htmlFor="username"
+              className="mb-2 block text-sm font-semibold text-gray-600"
+            >
+              Username
+            </label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={signupFormData.username}
+              onChange={signupHandle}
+              className="w-full rounded-md border border-gray-300 px-4 py-2 focus:border-blue-500 focus:outline-none"
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              htmlFor="password"
+              className="mb-2 block text-sm font-semibold text-gray-600"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={signupFormData.password}
+              onChange={signupHandle}
+              className="w-full rounded-md border border-gray-300 px-4 py-2 focus:border-blue-500 focus:outline-none"
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full rounded-md bg-blue-500 px-4 py-2 text-white hover:bg-blue-600 focus:bg-blue-600 focus:outline-none"
+          >
+            Sign Up
+          </button>
+        </form>
+      </div>
+      <div className="w-1/5">
+        <form
+          onSubmit={loginHandleSubmit}
+          className="w-full border-2 border-black p-4"
+        >
+          <div className="mb-4">
+            <label
+              htmlFor="username"
+              className="mb-2 block text-sm font-semibold text-gray-600"
+            >
+              Username
+            </label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={loginFormData.username}
+              onChange={loginHandle}
+              className="w-full rounded-md border border-gray-300 px-4 py-2 focus:border-blue-500 focus:outline-none"
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              htmlFor="password"
+              className="mb-2 block text-sm font-semibold text-gray-600"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={loginFormData.password}
+              onChange={loginHandle}
+              className="w-full rounded-md border border-gray-300 px-4 py-2 focus:border-blue-500 focus:outline-none"
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full rounded-md bg-blue-500 px-4 py-2 text-white hover:bg-blue-600 focus:bg-blue-600 focus:outline-none"
+          >
+            Log In
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
-
 export default SignupForm;
--- src/server/api/auth.ts
+++ src/server/api/auth.ts
@@ -1,5 +1,68 @@
 import {z} from 'zod';
+import crypto from "crypto";
+import { TRPCError } from '@trpc/server';
 import {createTRPCRouter, publicProcedure} from '~/server/api/trpc';
 
 export const authRouter = createTRPCRouter({
+    signupMutation: publicProcedure.input(z.object({username: z.string(), password: z.string()})).mutation(async ({ctx, input}) => {
+       const {username, password} = input;
+       if (username && password){
+        const user = await ctx.db.user.findFirst({where: {username: username}})
+        if (user){
+            throw new TRPCError({
+                code: "BAD_REQUEST", 
+                message: "username already taken"
+            })
+        }else{
+           const salt = hash(username + new Date().getTime());
+           const passwordHash = hash(password + salt)
+
+           const newUser = await ctx.db.user.create({data: {username: username, passwordHash: passwordHash, salt: salt}})
+           return {
+            success: true,
+            newuser: newUser
+           }
+        }
+       }else{
+         throw new TRPCError({
+            code: "BAD_REQUEST", 
+            message: "no username or password"
+         })
+       }
+    }),
+    loginMutation: publicProcedure.input(z.object({username: z.string(), password: z.string()})).mutation(async ({ctx, input}) => {
+        const {username, password} = input;
+        if (username && password){
+            const user = await ctx.db.user.findFirst({where: {username: username}})
+            if (user){
+                const passwordHash = hash(password + user.salt)
+                if (passwordHash === user.passwordHash){
+                    return {
+                        token: hash(username + new Date().getTime())
+                    }
+                }else{
+                    throw new TRPCError({
+                        code: "BAD_REQUEST", 
+                        message: "wrong password"
+                    })
+                }
+            }else{
+                throw new TRPCError({
+                    code: "BAD_REQUEST", 
+                    message: "no such user"
+                })
+            }
+        }else{
+            throw new TRPCError({
+                code: "BAD_REQUEST", 
+                message: "no username or password"
+            })
+        }
+    }),
 });
+
+
+export function hash(input: string){
+    const hash = crypto.createHash('sha256').update(input).digest('base64')
+    return hash;
+}
